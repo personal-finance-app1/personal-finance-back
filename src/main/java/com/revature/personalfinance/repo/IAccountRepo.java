5d3956ac2164a501b58b13a8df086234e31ad645
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-12 13:42:28 -0600

    Initial setup

---
+++ src/main/java/com/revature/personalfinance/repo/IAccountRepo.java
@@ -0,0 +1,18 @@
+package com.revature.personalfinance.repo;
+
+import com.revature.personalfinance.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface IAccountRepo extends JpaRepository<Account, Integer> {
+
+    Account getAccountByAccountId(int accountId);
+    List<Account> getAccountsByUserId(int userId);
+
+    //Account updateAccount(Account account);
+
+    // For Stretch goal (multiple tracked accounts)
+    // void deleteAccountByAccountId(int accountId);
+}
