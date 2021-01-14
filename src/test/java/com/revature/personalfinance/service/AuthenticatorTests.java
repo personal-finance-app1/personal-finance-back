f9f0a4b2c3237a1f109b36342f988b1a6387ee01
Kevin Hake <kevin.hake@gmail.com>
2021-01-14 13:34:17 -0500

    updated pom file and added comments to auth test file.

--- src/test/java/com/revature/personalfinance/service/AuthenticatorTests.java
+++ src/test/java/com/revature/personalfinance/service/AuthenticatorTests.java
@@ -6,61 +6,74 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.runners.MockitoJUnitRunner;
 
-//@RunWith(MockitoJUnitRunner.class)
 public class AuthenticatorTests {
 
-	Authenticator authTest = new Authenticator();
-	String validJWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.N6_Glh0-bHSxjiZOgmIY6By_sog63l7bHRlxbExYPY8";
-	String invalidJWT = "eyJhbGci0iJTUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIi0iIxMjM0NTY30DkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaXNTb2NpYWwiOnRydWV9.4pcPyMD09olPSyXnrXcjTwXyr4BsezdI1AVTmud2fU4";
+	// We will need more accurate JWTs that we can manipulate before being able to complete the testing
+	String validJWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+			+ "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ."
+			+ "N6_Glh0-bHSxjiZOgmIY6By_sog63l7bHRlxbExYPY8";
+	String invalidJWT = "eyJhbGci0iJTUzI1NiIsInR5cCI6IkpXVCJ9."
+			+ "eyJzdWIi0iIxMjM0NTY30DkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaXNTb2NpYWwiOnRydWV9."
+			+ "4pcPyMD09olPSyXnrXcjTwXyr4BsezdI1AVTmud2fU4";
 
-	// Check valid JWT
+	/**
+	 * Method for testing isAuthentic() with a valid JWT
+	 */
 	@Test
 	public void testIsAuthenticSuccess() {
-		boolean result = authTest.isAuthentic(validJWT);
-		System.out.println(("Valid user DOB: " + authTest.toString()));
+		boolean result = Authenticator.isAuthentic(validJWT);
+		System.out.println("Authentic JWT case: " + validJWT);
 		assertTrue(result);
 	}
 
-	// Check invalid JWT
+	/**
+	 * Method for testing isAuthentic() with an invalid JWT
+	 */
 	@Test
 	public void testIsAuthenticFail1() {
-		boolean result = authTest.isAuthentic(invalidJWT);
-		System.out.println(("Invalid user DOB: " + authTest.toString()));
+		boolean result = Authenticator.isAuthentic(invalidJWT);
+		System.out.println("Invalid JWT case: " + invalidJWT);
 		assertFalse(result);
 	}
 
-	// Check invalid JWT
+	/**
+	 * Method for testing isAuthentic() with an empty string for JWT
+	 */
 	@Test
 	public void testIsAuthenticFail2() {
-		boolean result = authTest.isAuthentic("");
-		System.out.println(("Invalid user DOB: " + authTest.toString()));
+		boolean result = Authenticator.isAuthentic("");
+		System.out.println("Empty String JWT case. ");
 		assertFalse(result);
 	}
 
-	// Check valid getUsername method
+	/**
+	 * Method for testing getUserName() with a valid JWT username
+	 */
 	@Test
 	public void testUsernameSuccess() {
-		String result = authTest.getUsername("valid jwt");
-		System.out.println(("Valid user DOB: " + authTest.toString()));
+		String result = Authenticator.getUsername(validJWT);
+		System.out.println("Valid getUsername case: " + validJWT);
 		assertEquals("validName", result);
 	}
 
-	// Check invalid getUsername method
+	/**
+	 * Method for testing getUserName() with an invalid JWT username
+	 */
 	@Test
 	public void testUsernameFail1() {
-		String result = authTest.getUsername("valid jwt");
-		System.out.println(("Valid user DOB: " + authTest.toString()));
+		String result = Authenticator.getUsername(invalidJWT);
+		System.out.println("Invalid getUserName with invalid JWT: " + invalidJWT);
 		assertEquals("invalidName", result);
 	}
 
-	// Check invalid getUsername method
+	/**
+	 * Method for testing getUserName() with an empty string fro JWT
+	 */
 	@Test
 	public void testUsernameFail2() {
-		String result = authTest.getUsername("");
-		System.out.println(("Valid user DOB: " + authTest.toString()));
+		String result = Authenticator.getUsername("");
+		System.out.println("Invalid getUsername with empty JWT.");
 		assertNull(result);
 	}
 
