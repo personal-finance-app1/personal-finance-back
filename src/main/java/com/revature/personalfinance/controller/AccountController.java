32d553b92aea434519c8c97a312536c9632d2e38
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-25 10:21:39 -0600

    Added a controller to test responses on the ec2

--- src/main/java/com/revature/personalfinance/controller/AccountController.java
+++ src/main/java/com/revature/personalfinance/controller/AccountController.java
@@ -1,2 +1,20 @@
-package com.revature.personalfinance.controller;public class AccountController {
+
+package com.revature.personalfinance.controller;
+
+import com.revature.personalfinance.model.Account;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping
+    public ResponseEntity<Account> getAccount(int uid) {
+        Account testAccount = new Account(12, 0, "Checking", 2100, 1200);
+        return ResponseEntity.status(HttpStatus.OK).body(testAccount);
+    }
 }
