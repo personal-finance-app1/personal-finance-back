655f9906e646bc9ffe7fee51f589a344fa925632
magnusjw <magnusjw@clarkson.edu>
2021-01-25 17:01:19 -0500

    Cross Origin in Controller

--- src/main/java/com/revature/personalfinance/controller/AccountController.java
+++ src/main/java/com/revature/personalfinance/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.revature.personalfinance.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.revature.personalfinance.service.IAccountService;
 
 @RestController
 @RequestMapping("/accounts")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AccountController {
     
     private IAccountService accountService;
