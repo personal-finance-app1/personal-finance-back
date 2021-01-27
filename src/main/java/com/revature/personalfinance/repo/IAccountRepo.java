5b85935f6f3ce455114fb67105fa4a6249d94874
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-27 00:16:37 -0600

    More documentation fixes

--- src/main/java/com/revature/personalfinance/repo/IAccountRepo.java
+++ src/main/java/com/revature/personalfinance/repo/IAccountRepo.java
@@ -10,7 +10,7 @@ public interface IAccountRepo extends JpaRepository<Account, String> {
     List<Account> getAccountsByUserId(int userId);
     /** findByName returns a list of all accounts assigned to user with username name
      * @param userId the name of the user whose List of accounts we'd like to fetch
-     * @return List<Account> of all Accounts belonging to user with String name
+     * @return List of all Accounts belonging to user with userId
      */
     List<Account> findAllByUserId(String userId);
     Account findByAccountId(Integer accountId);
