942a4ccf8401ad0a36549cb206091510703e1373
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-27 21:55:35 -0600

    authentication working

--- src/main/java/com/revature/personalfinance/controller/AccountController.java
+++ src/main/java/com/revature/personalfinance/controller/AccountController.java
@@ -1,14 +1,14 @@
 package com.revature.personalfinance.controller;
 
+import com.google.api.Http;
+import com.google.api.HttpOrBuilder;
+import com.revature.personalfinance.service.Authenticator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,18 +20,23 @@ import com.revature.personalfinance.service.IAccountService;
 @CrossOrigin(origins = "http://localhost:4200")
 public class AccountController {
     
-    private IAccountService accountService;
+    private final IAccountService accountService;
+    private final Authenticator authenticator;
+
+    private static final Logger log = LogManager.getLogger();
     
     @Autowired
-    public AccountController(IAccountService accountService) {
+    public AccountController(IAccountService accountService, Authenticator authenticator) {
         super();
         this.accountService = accountService;
+        this.authenticator = authenticator;
     }
     
     @PatchMapping()
-    public ResponseEntity<Account> updateAccount(@RequestBody Account account) {
+    public ResponseEntity<Account> updateAccount(@RequestHeader(name = "Authorization") String jwt, @RequestBody Account account) {
         ResponseEntity<Account> returnEntity = ResponseEntity.status(400).body(null);
         Account persistedAccount = null;
+        if(!authenticator.isAuthentic(jwt)) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 
         if(this.accountService == null) {
             returnEntity = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
@@ -47,16 +52,23 @@ public class AccountController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Account>> getAllAccountsByUser(@RequestBody Account userAccount){
+    public ResponseEntity<List<Account>> getAllAccountsByUser(@RequestHeader(name = "Authorization") String jwt){
+        log.info(jwt);
+        String userId = "";
+        userId = authenticator.getUserId(jwt);
+        String message = String.format("userId: %s", userId);
+        log.info(message);
+
         ResponseEntity<List<Account>> returnEntity = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        if (!authenticator.isAuthentic(jwt)) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+
         List<Account> returnList = null;
-        String userId = userAccount.getUserId();
+
 
         if(this.accountService == null) {
             returnEntity = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
-        else if(userAccount != null && userAccount.getUserId() != null && !userAccount.getUserId().equals("")){
-            userId = userAccount.getUserId();
+        else if(userId != null && !userId.equals("")) {
             returnList = this.accountService.getAllAccountsByUserId(userId);
             returnEntity = ResponseEntity.status(200).body(returnList);
         }
--- src/test/java/com/revature/personalfinance/controller/AccountControllerTest.java
+++ src/test/java/com/revature/personalfinance/controller/AccountControllerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import com.revature.personalfinance.service.Authenticator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +20,7 @@ import com.revature.personalfinance.service.IAccountService;
 @RunWith(MockitoJUnitRunner.class)
 public class AccountControllerTest {
 	public AccountController accountController;
+	public Authenticator authenticator;
     @Mock
     public IAccountService accountService;
     public Account account;
@@ -27,8 +29,8 @@ public class AccountControllerTest {
     @Before
     public void setUp() {
         objectMapper = new ObjectMapper();
-        
-        accountController = new AccountController(accountService);
+        authenticator = new Authenticator();
+        accountController = new AccountController(accountService, authenticator);
         account = new Account(1,"test-user","test",1,1);
         when(accountService.updateAccount(any())).thenReturn(null);
         when(accountService.updateAccount(account)).thenReturn(account);
@@ -38,20 +40,20 @@ public class AccountControllerTest {
     
     @Test
     public void testUpdateAccountSuccess() {
-        ResponseEntity<Account> returnedAccount = accountController.updateAccount(account);
+        ResponseEntity<Account> returnedAccount = accountController.updateAccount("", account);
         assertEquals(account,objectMapper.convertValue(returnedAccount.getBody(), Account.class));
     }
     
     @Test
     public void testUpdateAccountFail() {
         Account badAccount = new Account(1000,"invalid-user","NotInDB",1000,1000);
-		ResponseEntity<Account> returnedAccount = accountController.updateAccount(badAccount);
+		ResponseEntity<Account> returnedAccount = accountController.updateAccount("", badAccount);
         assertNotEquals(account,objectMapper.convertValue(returnedAccount.getBody(), Account.class));
     }
     
     @Test
     public void testUpdateAccountNull() {
-    	ResponseEntity<Account> returnedAccount = accountController.updateAccount(null);
+    	ResponseEntity<Account> returnedAccount = accountController.updateAccount("", null);
         assertEquals(null,objectMapper.convertValue(returnedAccount.getBody(), Account.class));
     }
 }
